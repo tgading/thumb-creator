85bf531a0d35b51a0a2e62dbaf8aea92751134d2
Thorsten Gading <thorsten.gading@babor.de>
2023-11-15 08:28:55 +0000

    first commit

---
+++ src/Thumb.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tossn\ThumbCreator;
+
+/**
+ */
+final class Thumb
+{
+
+    /**
+     */
+    public function __construct()
+    {
+        echo '<pre style="text-align:left;">';var_dump(999);echo '</pre>'; exit;
+    }
+
+}
